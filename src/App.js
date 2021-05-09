b08ea28a39ba76c002dfe17017ecc86a1be38433
Masingita <masinge322@gmail.com>
2021-05-09 19:25:36 +0200

    chat web app completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import './App.css';
 import {ChatEngine} from 'react-chat-engine' 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine 
       height="100vh"
       projectID="97da2342-c06b-4256-a37b-247e44245f6f"
-      userName="abelmasingita"
-      userSecret="123456"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed  {...chatAppProps}/>}
     />
   );
